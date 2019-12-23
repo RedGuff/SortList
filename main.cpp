8036184cf11f784ce1dd6667271dbbca0fb59b1a
Saussereau Dominique <domsau2@yahoo.fr>
2019-12-23 07:26:50 +0100

    Letters are used (ASCII). Before is now int.
    Letters are used, not only thee size of the string.
    Before is now int instead of bool.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <string>
 #include <fstream>
-#include <time.h>
 using namespace std;
 
 string file = "List.txt";
@@ -10,15 +9,68 @@ string fileSorted = "List.txt"; // "List-sorted.txt";
 string fileBak = "List.txt_bak";
 vector<string> Liste1(0, "");
 
-bool FirstBefore(string world1 = "", string world2 = "") {
-    if(world1.size() <= world2.size()) { // Important : le cas "égal" ne doit pas boucler !
-        return true;
+int FirstBeforeLong(string world1 = "", string world2 = "") { // Ok.
+    if(world1.size() < world2.size()) {
+        return 1;
+        }
+    else if(world1.size() == world2.size()) { // Important : le cas "égal" ne doit pas boucler !
+        return 0;
         }
     else {
-        return false;
+        return -1;
         }
     }
 
+
+int FirstBeforeLetterASCII(char a = 'a', char b = 'b') {
+    if(int(a) < int(b)) {
+        return 1;
+        }
+    else if(int(a) > int(b)) {
+        return -1;
+        }
+    else {
+        return 0; // Important : le cas "égal" ne doit pas boucler !
+        }
+    }
+
+int FirstBeforeLetter(char a = 'a', char b = 'b'){
+return FirstBeforeLetterASCII(a,b);
+}
+
+int FirstBeforeWord(string world1 = "", string world2 = "") { // https://fr.wikipedia.org/wiki/Classement_alphab%C3%A9tique#Principes_de_classement
+    for(int letter = 0; letter < min(world1.size(), world2.size()); letter++) { // While?
+        if(FirstBeforeLetter(world1[letter], world2[letter]) == -1) {
+            return -1;
+            }
+        if(FirstBeforeLetter(world1[letter], world2[letter]) == 1) {
+            return 1;
+            }
+        /*
+        if(FirstBeforeLetter(world1[letter], world2[letter]) == 0) {
+            break; // Next letter;
+            }
+
+        */
+        }
+    return FirstBeforeLong(world1, world2); // :
+    /*
+     if(world1.size() == world2.size()) { // Identiques.
+         return 0;
+         }
+     if(world1.size() < world2.size()) { // Si identiques + suite : le plus court avant.
+         return 1;
+         }
+     else {
+         return -1;
+         }
+    */
+    }
+
+int FirstBefore(string world1 = "", string world2 = "")  {   // Differents tris possibles.
+    // return FirstBeforeLong(world1,  world2);
+    return FirstBeforeWord(world1,  world2 );
+    }
 
 void readListe1() { // Ok.
     for(int a = 0; a < Liste1.size(); a++) {
@@ -31,8 +83,8 @@ void sortList1() { // Mix Liste1, via une liste temporaire. // Ok.
     string tempString = "";
     do {
         SortFinished = true;
-        for(int a = 0; a < Liste1.size() - 1; a++) { // aMax fois, aMax non utilisé. parfait pour les tableaux.
-            if(!FirstBefore(Liste1[a], Liste1[a + 1])) {
+        for(int a = 0; a < Liste1.size() - 1; a++) {
+            if(FirstBefore(Liste1[a], Liste1[a + 1]) == -1) {
                 SortFinished = false;
 swapElementsListe:
                 tempString = Liste1[a];
@@ -43,7 +95,6 @@ swapElementsListe:
         }
     while(SortFinished == false);
     }
-
 
 void initListe(string fileName1 = file) {   // With BAK. // Ok.
     ifstream file(fileName1.c_str(), ios::in);
@@ -64,7 +115,7 @@ void initListe(string fileName1 = file) {   // With BAK. // Ok.
         }
     }
 
-void writeListe1(string fileOut = "out.txt") {
+void writeListe1(string fileOut = "out.txt") { // Ok.
     ofstream outfile(fileOut.c_str());     // Pas d'append : on remplace ! On a déjà sauvé avant.
     if(!outfile) {
         cerr << "Impossible to write the file: " << fileOut << endl;
@@ -79,13 +130,12 @@ void writeListe1(string fileOut = "out.txt") {
     }
 
 int main() {
-    srand(time(NULL));       // No need for better init.
     initListe();
-//    readListe1(); // Tests ok.
+    readListe1(); // Tests ok.
     sortList1();
     cout << " " << endl;
- //   readListe1(); // Tests ok.
-    writeListe1(fileSorted);
-    cout << "Ok." << endl;
+    readListe1(); // Tests ok.
+//    writeListe1(fileSorted);
+    cout << "\nOk." << endl;
     return 0;
     }
